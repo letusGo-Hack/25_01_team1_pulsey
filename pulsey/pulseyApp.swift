52a7e8704df57cc78bf2d92697559c1a6ad09422
Mason Kim <qwerty3345@naver.com>
2025-07-19 15:12:31 +0900

    add: 딥링크를 통해 운동 정보로 풀스크린 이동 구현

--- pulsey/pulseyApp.swift
+++ pulsey/pulseyApp.swift
@@ -11,25 +11,16 @@ import HealthKit
 @main
 struct pulseyApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    @State private var workouts: [HKWorkout] = []
-    @State private var deepLinkManager = DeepLinkManager.shared
 
     var body: some Scene {
         WindowGroup {
-            VStack {
-                if let workout = workouts.last {
-                    WorkoutDetailView(workout: workout)
-                } else {
-                    UserInfoView()
-                        .task {
-                            try? await HealthKitManager.shared.requestWorkoutAuthorization()
-                            self.workouts = (try? await HealthKitManager.shared.fetchWorkouts()) ?? []
-                        }
+            MainView()
+                .task {
+                    try? await HealthKitManager.shared.requestWorkoutAuthorization()
                 }
-            }
-            .onOpenURL { url in
-                DeepLinkManager.handleDeepLink(url)
-            }
+                .onOpenURL { url in
+                    DeepLinkManager.handleDeepLink(url)
+                }
         }
     }
 }
