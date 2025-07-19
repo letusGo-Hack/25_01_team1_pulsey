c05bffbf8a0b72a77e0dce52b8654ca48dddc9f1
Mason Kim <qwerty3345@naver.com>
2025-07-19 14:16:37 +0900

    chore: 시뮬레이터 환경에서는 mock 을 반환하게 처리

--- pulsey/Manager/HeatlhKitManager.swift
+++ pulsey/Manager/HeatlhKitManager.swift
@@ -1,3 +1,10 @@
+//
+//  HealthKitManager.swift
+//  pulsey
+//
+//  Created by Mason Kim on 7/19/25.
+//
+
 import Foundation
 import HealthKit
 
@@ -8,15 +15,35 @@ final class HealthKitManager {
 
     private init() {}
 
+    // 시뮬레이터 환경 확인
+    private var isSimulator: Bool {
+#if targetEnvironment(simulator)
+        return true
+#else
+        return false
+#endif
+    }
+
     // 운동 데이터 권한 요청
     func requestWorkoutAuthorization() async throws {
         guard HKHealthStore.isHealthDataAvailable() else { return }
+
+        // 시뮬레이터에서는 권한 요청을 스킵
+        if isSimulator {
+            return
+        }
+
         let workoutType = HKObjectType.workoutType()
         try await store.requestAuthorization(toShare: [], read: [workoutType])
     }
 
     // 운동 데이터 받아오기
     func fetchWorkouts() async throws -> [HKWorkout] {
+        // 시뮬레이터 환경에서는 목데이터 반환
+        if isSimulator {
+            return .mock()
+        }
+
         let workoutType = HKObjectType.workoutType()
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
         let predicate: NSPredicate? = nil
---
+++ pulsey/Model/HKWorkout+Mock.swift
@@ -0,0 +1,105 @@
+//
+//  HKWorkout+Mock.swift
+//  pulsey
+//
+//  Created by Mason Kim on 7/19/25.
+//
+
+import Foundation
+
+import HealthKit
+
+extension [HKWorkout] {
+    static func mock() -> [HKWorkout] {
+        let calendar = Calendar.current
+        let now = Date()
+
+        var mockWorkouts: [HKWorkout] = []
+
+        // 러닝 운동 데이터
+        let runningStartDate = calendar.date(byAdding: .hour, value: -2, to: now)!
+        let runningEndDate = calendar.date(byAdding: .minute, value: -90, to: now)!
+        let runningWorkout = HKWorkout(
+            activityType: .running,
+            start: runningStartDate,
+            end: runningEndDate,
+            duration: 1800, // 30분
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 250),
+            totalDistance: HKQuantity(unit: .meter(), doubleValue: 5000),
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(runningWorkout)
+
+        // 사이클링 운동 데이터
+        let cyclingStartDate = calendar.date(byAdding: .day, value: -1, to: now)!
+        let cyclingEndDate = calendar.date(byAdding: .day, value: -1, to: calendar.date(byAdding: .minute, value: 45, to: cyclingStartDate)!)!
+        let cyclingWorkout = HKWorkout(
+            activityType: .cycling,
+            start: cyclingStartDate,
+            end: cyclingEndDate,
+            duration: 2700, // 45분
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 320),
+            totalDistance: HKQuantity(unit: .meter(), doubleValue: 15000),
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(cyclingWorkout)
+
+        // 수영 운동 데이터
+        let swimmingStartDate = calendar.date(byAdding: .day, value: -2, to: now)!
+        let swimmingEndDate = calendar.date(byAdding: .minute, value: 60, to: swimmingStartDate)!
+        let swimmingWorkout = HKWorkout(
+            activityType: .swimming,
+            start: swimmingStartDate,
+            end: swimmingEndDate,
+            duration: 3600, // 60분
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 400),
+            totalDistance: HKQuantity(unit: .meter(), doubleValue: 2000),
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(swimmingWorkout)
+
+        // 근력 운동 데이터 (거리 정보 없음)
+        let strengthStartDate = calendar.date(byAdding: .day, value: -3, to: now)!
+        let strengthEndDate = calendar.date(byAdding: .minute, value: 50, to: strengthStartDate)!
+        let strengthWorkout = HKWorkout(
+            activityType: .traditionalStrengthTraining,
+            start: strengthStartDate,
+            end: strengthEndDate,
+            duration: 3000, // 50분
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 280),
+            totalDistance: nil,
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(strengthWorkout)
+
+        // 요가 운동 데이터
+        let yogaStartDate = calendar.date(byAdding: .day, value: -4, to: now)!
+        let yogaEndDate = calendar.date(byAdding: .minute, value: 75, to: yogaStartDate)!
+        let yogaWorkout = HKWorkout(
+            activityType: .yoga,
+            start: yogaStartDate,
+            end: yogaEndDate,
+            duration: 4500, // 75분
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 180),
+            totalDistance: nil,
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(yogaWorkout)
+
+        // 하이킹 운동 데이터
+        let hikingStartDate = calendar.date(byAdding: .day, value: -5, to: now)!
+        let hikingEndDate = calendar.date(byAdding: .hour, value: 2, to: hikingStartDate)!
+        let hikingWorkout = HKWorkout(
+            activityType: .hiking,
+            start: hikingStartDate,
+            end: hikingEndDate,
+            duration: 7200, // 2시간
+            totalEnergyBurned: HKQuantity(unit: .kilocalorie(), doubleValue: 600),
+            totalDistance: HKQuantity(unit: .meter(), doubleValue: 8000),
+            metadata: [HKMetadataKeyWorkoutBrandName: "Mock Fitness App"]
+        )
+        mockWorkouts.append(hikingWorkout)
+
+        return mockWorkouts
+    }
+}
