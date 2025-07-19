c3a305f765d70d0512b2bcea93dbd85244dcb093
JJC <qppl950@gmail.com>
2025-07-19 16:30:57 +0900

    *

--- pulsey/Manager/RoutineManager.swift
+++ pulsey/Manager/RoutineManager.swift
@@ -8,21 +8,6 @@
 import FoundationModels
 import Observation
 
-//let sports = [
-//    Sport(id: 0, name: "러닝", imageName: "figure.run"),
-//    Sport(id: 1, name: "헬스", imageName: "dumbbell.fill"),
-//    Sport(id: 2, name: "요가", imageName: "figure.mind.and.body"),
-//    Sport(id: 3, name: "수영", imageName: "figure.pool.swim"),
-//    Sport(id: 4, name: "자전거", imageName: "bicycle"),
-//    Sport(id: 5, name: "등산", imageName: "mountain.2.fill"),
-//    Sport(id: 6, name: "테니스", imageName: "tennis.racket"),
-//    Sport(id: 7, name: "축구", imageName: "soccerball"),
-//    Sport(id: 8, name: "농구", imageName: "basketball.fill"),
-//    Sport(id: 9, name: "배드민턴", imageName: "figure.badminton"),
-//    Sport(id: 10, name: "복싱", imageName: "figure.boxing"),
-//    Sport(id: 11, name: "필라테스", imageName: "figure.core.training")
-//]
-
 @Observable
 @MainActor
 final class RoutinePlanner {
